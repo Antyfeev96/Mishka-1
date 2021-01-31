b9aae9bb18cfe5868ff8d9086545abe345867ad5
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-31 17:49:23 +0300

    Initial commit

---
+++ src/TaskList.js
@@ -0,0 +1,15 @@
+const TaskList = () => {
+  return (
+    <div>
+      <ul>
+        Список дел на сегодня
+        <li>Первая задача</li>
+        <li>Вторая задача</li>
+        <li>Третья задача</li>
+        <li>Четвёртая задача</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
